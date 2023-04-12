129d0ca8d7775452b4f5c4c42ece81e496127062
dename87 <87smaxim@gmail.com>
2023-04-12 17:04:43 +0600

    feat: solve transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let res = [];
+  const commands = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  if (!Array.isArray(arr)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (!commands.includes(arr[i])) {
+      res.push(arr[i]);
+    } else  {
+        if (arr[i] === '--discard-next' && i !== arr.length - 1) {
+          i += 1;
+          res.push('-');
+    } else {
+        if (arr[i] === '--discard-prev' && i !== 0) {
+          res.pop();
+    } else {
+        if (arr[i] === '--double-next' && i !== arr.length - 1) {
+          res.push(arr[i + 1]);
+    } else {
+        if (arr[i] === '--double-prev' && i !== 0) {
+          res.push(res[res.length - 1]);
+        }
+        }
+        }
+        }
+    }
+  }
+  res = res.filter(item => item !== '-');
+  return res;
 }
 
 module.exports = {
