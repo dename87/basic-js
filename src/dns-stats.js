c61642daab5cb7b16e48a61cf480e04eabeb93dc
dename87 <87smaxim@gmail.com>
2023-04-13 11:12:35 +0600

    feat: solve dns-stats.js

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {};
+  let tmpElem;
+  let tmpStr = '';
+  for (let elem of domains) {
+    tmpElem = elem.split('.').reverse();
+    for (let elems of tmpElem) {
+      tmpStr += '.' + elems;
+      if (Object.keys(obj).includes(tmpStr)) {
+        obj[tmpStr] += 1;
+      } else {
+          obj[tmpStr] = 1;
+      }
+    }
+    tmpStr = '';
+  }
+  return obj;
 }
 
 module.exports = {
